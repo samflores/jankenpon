71baed5f4d284d4aac3c2036cf5be6cc411c6006
Samuel Flores <me@samflor.es>
2022-12-07 13:42:29 -0300

    Dry it up a litte bit

--- main.rb
+++ main.rb
@@ -8,16 +8,18 @@ SHAPES = [
   Spock.new
 ].freeze
 
+OUTCOMES = {
+  victory: 'wins against',
+  defeat: 'loses against',
+  draw: 'ties with'
+}
+
 # 10 random sample rounds
 10.times do
   player1 = SHAPES.sample
   player2 = SHAPES.sample
 
-  message = case player1.versus(player2)
-  when :victory then "#{player1} wins against #{player2}"
-  when :defeat then "#{player1} loses against #{player2}"
-  when :draw then "#{player1} ties with #{player2}"
-  end
+  outcome = OUTCOMES[player1.versus(player2)]
 
-  puts message
+  puts "#{player1} #{outcome} #{player2}"
 end
